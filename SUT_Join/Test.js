c7fec0b737f26cbf9c9d82320cdd8ea99e6ed859
SuppanatThreerattananukul <46100073+SuppanatThreerattananukul@users.noreply.github.com>
2019-11-17 14:23:00 +0700

    Add project

---
+++ SUT_Join/Test.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { TextInput } from 'react-native';
+
+export default function UselessTextInput() {
+  const [value, onChangeText] = React.useState('');
+
+  return (
+    <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
+  );
+}
